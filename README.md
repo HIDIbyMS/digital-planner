a23e3737f95132e683076e2e49506a775b1a793a
Michelle <33467190+HIDIbyMS@users.noreply.github.com>
2022-06-23 15:34:40 -0400

    update for smaller screens

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# digital-planner
-adobe pdf embedded digital planner
+# Adobe PDF Embed API Example
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hidibyms/pen/ZErgbXP](https://codepen.io/hidibyms/pen/ZErgbXP).
+
+This example utilizes the Adobe PDF Embed API to display a PDF when clicking a button to trigger the viewer. GSAP is used to animate in the PDF container smoothly.
---
+++ dist/script.js
@@ -0,0 +1,36 @@
+const clientID = "abd1af088d86476fb223867c75c8fa02";
+let btn = document.querySelector('button.pdfActivate')
+
+// PDF API Initialization
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+  console.log('adobe_dc_view_sdk.ready')
+  btn.disabled = false;
+
+  let adobeDCView = new AdobeDC.View({
+    clientId: clientID,
+    divId: "adobe-dc-view"
+  });
+  // Wrap in function to call when needed
+  const displayPDF = () => {
+    // Prevent additional PDF button clicks
+    btn.disabled = true;
+
+    // Create PDF preview
+    adobeDCView.previewFile({
+      content: {location: {url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf"}},
+      metaData: { fileName: "Bodea Brochure.pdf" }
+    }, {});
+
+    // Animate PDF viewer & PDF
+    let pdfTL = gsap.timeline({})
+    pdfTL.to('#adobe-dc-view', {opacity:1, duration:0.125})
+    pdfTL.fromTo('#adobe-dc-view', {height:'0'}, {height:'37.5rem', duration:0.5})
+    pdfTL.to('#adobe-dc-view iframe', {opacity:1, duration:0.25})
+  }
+
+  // Button listener
+  btn.addEventListener('click', function() {
+    displayPDF()
+    console.log('clicked')
+  });
+});
